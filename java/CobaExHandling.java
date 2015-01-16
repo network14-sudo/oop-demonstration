d62774763b36ac4aac08f567678e9a21ff08f820
Nanang F. Rozi <nfrozy@gmail.com>
2015-01-16 14:47:11 +0700

    Move files to specified directory

--- java/CobaExHandling.java
+++
@@ -1,8 +0,0 @@
-public class CobaExHandling {
-
-    public static void main(String[] args) {
-        
-        ExHandledTransaction transaksi = new ExHandledTransaction();
-        transaksi.entri();
-    }
-}
